1d17b0a41d7831de89ecf79ab42899da7d34b2b1
Oleksii Dubyna <alex.dubyna@gmail.com>
2021-01-30 00:35:09 +0000

    added tracking of cumulative sums for investments

--- functions.py
+++ functions.py
@@ -12,22 +12,43 @@ def read_data():
     """
     
     #get list of files to read
-    
     file_names = [f for f in os.listdir(os.path.join('data')) if f.endswith('.csv')]
     
     #get a single dataframe with list of columns we need
     all_data = pd.DataFrame(columns = pd.read_csv(os.path.join('data', file_names[0]), sep=',', header='infer').columns)
     
     #read & combine files to single dataframe
-    
+    #first we get all fund names with open positions at any point of time
+    all_fund_names = pd.DataFrame(columns = pd.read_csv(os.path.join('data', file_names[0]), sep=',', header='infer').columns)
+
+    for f in file_names:
+        df_fund_names = pd.read_csv(os.path.join('data', f), sep= ',', header='infer', thousands=',')
+        df_fund_names.drop(df_fund_names.index[0], inplace = True)
+        all_fund_names = pd.concat([all_fund_names,df_fund_names])
+    fund_names = set(all_fund_names['Fund'].unique())
+
+    print ('You have/had investments in these funds: \n', fund_names)
+
+    #second, while reading all files content we will add extra rows with Fund name and 0 investments in a period 
+    #to historic data to do nice cumulative sums with pandas later
     for f in file_names:
         df = pd.read_csv(os.path.join('data', f), sep=',', header='infer', thousands=',')
         df.drop(df.index[0], inplace = True)
+        cur_period_fund_names = set(df['Fund'].unique())
+
+        #list of funds that are absent in current month transactions, but which we need to track 
+        diff_list = list(fund_names - cur_period_fund_names)
+
+        #add those extra rows where necessary
+        prc_dt = df['Unit Price Date'][1]
+        for fnd in diff_list:
+            #print (prc_dt, fnd)
+            new_row = {'Fund':fnd, 'Fund Currency':'GBP', 'Units':0, 'Unit Price Date': prc_dt, 'Value': 0}
+            df = df.append(new_row, ignore_index = True)
 
         all_data = pd.concat([all_data, df])
     
     #Sort transactions by date and restore broken index
-
     all_data["Unit Price Date"] = pd.to_datetime(all_data["Unit Price Date"])
     all_data.sort_values(by='Unit Price Date', ascending = True, inplace=True)
     all_data.reset_index(drop=True, inplace=True)    
@@ -129,8 +150,8 @@ def cumul_volumes(df):
     new_df['ValueCumul'] = 0
     for e in vlist:
         cur_df = df[df['Fund'] == e]
-        cur_df['UnitsCumul'] = cur_df['Units'].cumsum()
-        cur_df['ValueCumul'] = cur_df['Value'].cumsum()
+        cur_df['UnitsCumul'] = round(cur_df['Units'].cumsum(),2)
+        cur_df['ValueCumul'] = round(cur_df['Value'].cumsum(),2)
         new_df = pd.concat([new_df, cur_df])
         new_df.sort_values(by='Unit Price Date', ascending = True, inplace=True)
         new_df.reset_index(drop=True, inplace=True)
